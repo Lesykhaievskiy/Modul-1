6c527ec31557635d27496e157502dd69e3ee812c
Oleksii Haievskyi <lesukhaievskiy@gmail.com>
2024-03-15 09:08:18 +0200

    first commit

---
+++ Modul/modul-1.cs
@@ -0,0 +1,35 @@
+﻿
+public enum DaysOfWeek {
+    Monday, 
+    Tuesday,
+    Wednesday,
+    Thuersday,
+    Friday,
+    Saturday,
+    Sunday
+
+}
+
+
+public class CalendarEvent {
+    private string EventName {get; set;}
+    private DaysOfWeek EventDay {get; set;}
+
+    public CalendarEvent(string evName, DaysOfWeek evDay) {
+        EventName = evName;
+        EventDay = evDay;
+    }
+
+    public void PrintEventDetails() {
+        Console.WriteLine($"Подія {EventName} відбудеться в день {EventDay}");
+    }
+
+}
+
+
+ class Program {
+    static void Main() {
+        CalendarEvent event1 = new CalendarEvent("Birthday party", DaysOfWeek.Sunday);
+        event1.PrintEventDetails();
+    }
+}
